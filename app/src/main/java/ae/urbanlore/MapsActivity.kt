5a33d67426461f270a8cfcb7cb15c45493a9c5f5
JuiceVodka <nikolajsok@gmail.com>
2022-05-14 12:15:02 +0200

    Inital commit, basic google maps

---
+++ app/src/main/java/ae/urbanlore/MapsActivity.kt
@@ -0,0 +1,140 @@
+package ae.urbanlore
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
+import ae.urbanlore.databinding.ActivityMapsBinding
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    companion object{
+        private const val REQUEST_ID_LOCATION_PERMISSIONS = 8
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        binding.refresh.setOnClickListener{
+            showLastKnownLocation()
+        }
+    }
+
+    fun showLastKnownLocation(){
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ){
+            Log.d("Debug", "What ios going on here")
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission
+                    .ACCESS_COARSE_LOCATION) || ActivityCompat.shouldShowRequestPermissionRationale (this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Log.d("Debug", "snekbar")
+                Snackbar.make(
+                    binding.root,
+                    R.string.permission_location_rationale,
+                    Snackbar.LENGTH_INDEFINITE
+                ).setAction(R.string.ok) {
+                    ActivityCompat.requestPermissions(
+                        this@MapsActivity, arrayOf(
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ),
+                        REQUEST_ID_LOCATION_PERMISSIONS
+                    )
+                }.show()
+            } else{
+                ActivityCompat.requestPermissions(
+                    this@MapsActivity, arrayOf(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ),
+                    REQUEST_ID_LOCATION_PERMISSIONS
+                )
+            }
+        }else{
+            Log.d("DEBUG", "FUSED_LOCATIOn")
+            fusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+                if(location != null){
+                    mMap.addMarker(
+                        MarkerOptions()
+                            .position(LatLng(location.latitude, location.longitude))
+                            .title(
+                                LocalDateTime.now().format(
+                                    DateTimeFormatter.ofLocalizedDateTime(
+                                        FormatStyle.SHORT, FormatStyle.SHORT)))
+                    )
+                    val zoom_level = 15.0F
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), zoom_level))
+                    val long = "Longitude" + location.longitude.toString()
+                    val lat = "Latitude: " + location.latitude.toString()
+                    binding.longitude.text = long
+                    binding.latitude.text = lat
+
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            showLastKnownLocation()
+        }
+    }
+
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+}
