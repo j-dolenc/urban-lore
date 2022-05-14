bd1e3f878519693c33605fad40bd11bfa452a7e8
jurijdolenc <jd5436@student.uni-lj.si>
2022-05-15 01:45:09 +0200

    comment

--- app/src/main/java/ae/urbanlore/DetailsActivity.kt
+++ app/src/main/java/ae/urbanlore/DetailsActivity.kt
@@ -16,39 +16,39 @@ class DetailsActivity : AppCompatActivity() {
         binding = ActivityDetailsBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
-    fun getMarkerInfo(markerId:String):MutableMap<String,String>{
-        val res = mutableMapOf("title" to "test", "description" to "test", "imgString" to "test")
-        val query: ParseQuery<ParseObject> = ParseQuery.getQuery("lore")
-        query.whereEqualTo("objectId", markerId);
-        query.getFirstInBackground { obj, e ->
-            if (e == null) {
-
-                val long: String? = obj?.getString("longitude")
-                val lat: String? = obj?.getString("latitude")
-                val title: String? = obj?.getString("name")
-                val description: String? = obj?.getString("description")
-                val imgString: String? = obj?.getString("stringPicture");
-
-                if (long!=null && lat != null && title != null && description != null && imgString != null){
-
-                    res["title"] = title
-                    res["description"] = description
-                    res["imgString"] = imgString
-                    //val decoded = Base64.getDecoder().decode(imgString)
-                    //val img = BitmapFactory.decodeByteArray( decoded,0, decoded.size)
-                    //itemImage.setImageBitmap(img)
-
-                }
-
-            }
-
-            myArray = dataList.toTypedArray()
-
-
-            Log.d("DEBUG",myArray.toString())
-
-        }
-        return res
-    }
+//    fun getMarkerInfo(markerId:String):MutableMap<String,String>{
+//        val res = mutableMapOf("title" to "test", "description" to "test", "imgString" to "test")
+//        val query: ParseQuery<ParseObject> = ParseQuery.getQuery("lore")
+//        query.whereEqualTo("objectId", markerId);
+//        query.getFirstInBackground { obj, e ->
+//            if (e == null) {
+//
+//                val long: String? = obj?.getString("longitude")
+//                val lat: String? = obj?.getString("latitude")
+//                val title: String? = obj?.getString("name")
+//                val description: String? = obj?.getString("description")
+//                val imgString: String? = obj?.getString("stringPicture");
+//
+//                if (long!=null && lat != null && title != null && description != null && imgString != null){
+//
+//                    res["title"] = title
+//                    res["description"] = description
+//                    res["imgString"] = imgString
+//                    //val decoded = Base64.getDecoder().decode(imgString)
+//                    //val img = BitmapFactory.decodeByteArray( decoded,0, decoded.size)
+//                    //itemImage.setImageBitmap(img)
+//
+//                }
+//
+//            }
+//
+//            myArray = dataList.toTypedArray()
+//
+//
+//            Log.d("DEBUG",myArray.toString())
+//
+//        }
+//        return res
+//    }
 
 }
